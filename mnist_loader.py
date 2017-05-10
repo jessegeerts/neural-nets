ba8aeb67bca7a399ad2c0aef8756d9001bec61f1
jessegeerts <jesse.geerts.14@ucl.ac.uk>
2017-05-10 11:49:44 +0100

    Refactored mnist load functions into separate file

---
+++ mnist_loader.py
@@ -0,0 +1,37 @@
+import gzip
+import pickle
+
+import numpy as np
+from keras import utils as utils
+
+
+def make_mnist_subset(data, digits):
+    """
+    Takes a list of numbers and returns the part of the MNIST dataset corresponding to these numbers. In addition,
+    the labels are converted to a 1 out of 10 representation.
+
+    :param tuple data: e.g. train_data, validation_data or test_data
+    :param list digits: list of digits to be included in subset (will be converted into list if int is given)
+    :return: a Nx8=784 array of images and a Nx10 array of labels
+    """
+    if isinstance(digits, int):
+        digits = [digits]
+
+    idx = [np.squeeze(np.argwhere(data[1] == i)) for i in digits]
+    idx = np.sort(np.concatenate(idx, axis=0))
+
+    images = data[0][idx]
+    labels = utils.to_categorical(data[1][idx],num_classes=10) # convert labels to categorical
+    return images, labels
+
+
+def load_mnist():
+    """
+    Loads the MNIST handwritten digits dataset into three tuples training_data/
+
+    :return: Three tuples containing training data, validation data and test data
+    """
+    f = gzip.open('./data/mnist.pkl.gz')
+    training_data, validation_data, test_data = pickle.load(f, encoding='latin1')
+    f.close()
+    return training_data, validation_data, test_data
--- mnist_tf.py
+++ mnist_tf.py
@@ -1,50 +1,14 @@
-import gzip
 import numpy as np
-import pickle
-import matplotlib.pyplot as plt
 import tensorflow as tf
-import keras
 
-
-def load_mnist():
-    """
-
-    :return:
-    """
-    f = gzip.open('/home/jesse/Code/neural-networks-and-deep-learning/data/mnist.pkl.gz')
-    training_data, validation_data, test_data = pickle.load(f, encoding='latin1')
-    f.close()
-    return training_data, validation_data, test_data
-
+from mnist_loader import make_mnist_subset, load_mnist
 
 training_data, validation_data, test_data = load_mnist()
-
-
-def make_subset(data, digits):
-    """
-    Takes a list of numbers and returns the part of the MNIST dataset corresponding to these numbers.
-
-    :param tuple data: e.g. train_data, validation_data or test_data
-    :param list digits: list of digits to be included in subset (will be converted into list if int is given)
-    :return:
-    """
-    if isinstance(digits, int):
-        digits = [digits]
-    idx = [np.squeeze(np.argwhere(data[1] == i)) for i in digits]
-    idx = np.sort(np.concatenate(idx, axis=0))
-    return data[0][idx], data[1][idx]
-
-
-# train_x, train_y = make_subset(training_data, [0, 1])
-train_x = training_data[0]
-train_y = training_data[1]
-test_x = test_data[0]
-test_y = test_data[1]
 
 # Parameters
 learning_rate = 0.001
 training_epochs = 15
-batch_size = 100
+batch_size = 64
 display_step = 1
 
 # Network Parameters
@@ -60,6 +24,15 @@ y = tf.placeholder("float", [None, n_classes])
 
 # Create model
 def multilayer_perceptron(x, weights, biases):
+    """
+    This function takes in the input placeholder, weights and biases and returns the output tensor of a network with
+    two hidden ReLU layers, and an output layer with linear activation.
+
+    :param tf.placeholder x: Placeholder for input
+    :param dict weights: Dictionary containing Variables describing weights of each layer
+    :param dict biases: Dictionary containing Variables describing biases of each layer
+    :return: The activations of the output layer
+    """
     # Hidden layer with RELU activation
     layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
     layer_1 = tf.nn.relu(layer_1)
@@ -87,39 +60,71 @@ biases = {
 pred = multilayer_perceptron(x, weights, biases)
 
 # Define loss and optimizer
-# cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred[:,[0:2]], labels=y[:,[0:2]]))
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
 
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+slice_pred = tf.slice(pred, [0, 0], [-1, 2])
+slice_y = tf.slice(y, [0, 0], [-1, 2])
+
+cost_zero_one = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=slice_pred, labels=slice_y))
+
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost_zero_one)
 
 # Initializing the variables
 init = tf.global_variables_initializer()
 
+# TODO: change this into functions (train and test)
+
 # Launch the graph
-with tf.Session() as sess:
+mySess = tf.Session()
+
+with mySess as sess:
     sess.run(init)
 
-    # Training cycle
-    for epoch in range(training_epochs):
-        avg_cost = 0.
-        total_batch = training_data[1].shape[0] / batch_size
-        # Loop over all batches
-        batches = [(train_x[k: k + batch_size], train_y[k: k + batch_size]) for k in range(0, len(train_y), batch_size)]
+    scores = np.full(5,np.NaN)
 
-        for batch in batches:
-            # Run optimization op (backprop) and cost op (to get loss value)
-            _, c = sess.run([optimizer, cost], feed_dict={x: batch[0],
-                                                          y: keras.utils.to_categorical(batch[1], num_classes=10)})
-            # Compute average loss
-            avg_cost += c / total_batch
-        # Display logs per epoch step
-        if epoch % display_step == 0:
-            print("Epoch:", '%04d' % (epoch + 1), "cost=", \
-                  "{:.9f}".format(avg_cost))
-    print("Optimization Finished!")
+    for task in range(0,10,2):
 
-    # Test model
-    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-    # Calculate accuracy
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-    print("Accuracy:", accuracy.eval({x: test_x, y: keras.utils.to_categorical(test_y, num_classes=10)}))
+        # take subset of MNIST data
+
+        train_x, train_y = make_mnist_subset(training_data, [task, task + 1])
+        test_x, test_y = make_mnist_subset(test_data, [task, task + 1])
+
+        # update loss and optimizer:
+        slice_pred = tf.slice(pred, [0, task], [-1, 2])
+        slice_y = tf.slice(y, [0, task], [-1, 2])
+
+        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=slice_pred, labels=slice_y))
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+        # Training cycle
+        for epoch in range(training_epochs):
+            avg_cost = 0.
+            total_batch = training_data[1].shape[0] / batch_size
+            # Loop over all batches
+            batches = [(train_x[k: k + batch_size], train_y[k: k + batch_size]) for k in range(0, len(train_y), batch_size)]
+
+            for batch in batches:
+                # Run optimization op (backprop) and cost op (to get loss value)
+                _, c = sess.run([optimizer, cost], feed_dict={x: batch[0],
+                                                                       y: batch[1]})
+                # Compute average loss
+                avg_cost += c / total_batch
+            # Display logs per epoch step
+            if epoch % display_step == 0:
+                print("Epoch:", '%04d' % (epoch + 1), "cost=","{:.9f}".format(avg_cost))
+        print("Optimization finished")
+
+        # Test model
+        correct_prediction = tf.equal(tf.argmax(slice_pred, 1), tf.argmax(slice_y, 1))
+        prediction = tf.argmax(pred, 1)
+        print("prediction:", prediction.eval({x: test_x, y: test_y}))
+
+        # TODO: test with only one head
+        # TODO: evaluate with test data
+
+        # print("prediction: ", tf.cast(tf.argmax(pred,1),"float"))
+        # Calculate accuracy
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+        print("Accuracy:", accuracy.eval({x: test_x, y: test_y}))
+
+        #print("Accuracy (training data):", accuracy.eval({x: train_x[0:batch_size], y: train_y[0:batch_size]}))
